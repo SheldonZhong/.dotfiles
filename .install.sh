6f2c25e86df65d2038372213c5d84f79036074d0
Wenshao Zhong <wzhong20@uic.edu>
2024-05-05 00:46:04 -0500

    install will init submodule and install vim plugins

--- .install.sh
+++ .install.sh
@@ -4,3 +4,8 @@ git config status.showUntrackedFiles no
 git init --separate-git-dir=$HOME/.dotfiles
 rsync --recursive --verbose --exclude '.git' ./ $HOME/
 
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule init
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update
+
+vim +PluginInstall +qall
+
